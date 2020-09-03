567298600212493f47c064857eaad0c9ced2c993
DeAdFrOnt <semenukmaks@gmail.com>
2020-09-03 11:24:45 +0300

    merge: 1-1

--- src/Todo/TodoList.js
+++ src/Todo/TodoList.js
@@ -1,10 +1,21 @@
 import React from "react";
 import TodoItem from "./TodoItem";
 
+const styles = {
+  ul: {
+    listStyle: "none",
+    color: "#fff",
+    justifyContent: "center",
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+  },
+};
+
 function TodoList() {
-    
+
   return (
-    <ul>
+    <ul style={styles.ul}>
       <TodoItem />
       <TodoItem />
       <TodoItem />

bd3b42d17fe8cb4b2c09f16e6240f20651592be6
DeAdFrOnt <semenukmaks@gmail.com>
2020-09-03 16:54:28 +0300

    merge: 1-4

--- src/Todo/TodoItem.js
+++ src/Todo/TodoItem.js
@@ -1,10 +1,21 @@
 import React from "react";
+import "./TodoItem.css";
 
 function TodoItem({ todo, index, onChange }) {
+  const classes = [];
+
+  if (todo.completed) {
+    classes.push("done");
+  }
+
   return (
-    <li>
+    <li className={classes.join(" ")}>
       <span>
-        <input type="checkbox" onChange={() => onChange(todo.id)} />
+        <input
+          type="checkbox"
+          onChange={() => onChange(todo.id)}
+          checked={todo.completed}
+        />
         <strong>{index + 1}</strong>
         {todo.title}
       </span>

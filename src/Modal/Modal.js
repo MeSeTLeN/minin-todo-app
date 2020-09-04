261e11de03592f289f3a17f7786f51e27ad28cc4
DeAdFrOnt <semenukmaks@gmail.com>
2020-09-04 21:49:35 +0300

    feat: close open btn, two ways

--- src/Modal/Modal.js
+++ src/Modal/Modal.js
@@ -2,17 +2,27 @@ import React, { Component } from "react";
 import "./Modal.css";
 
 export default class Modal extends Component {
+  state = {
+    isOpen: false,
+  };
+  openModal() {
+    this.setState({ isOpen: true });
+  }
   render() {
     return (
       <React.Fragment>
-        <button>Open modal</button>
-        <div className="modal">
-          <div className="modal-body">
-            <h1>Modal title</h1>
-            <p>Modal text</p>
-            <button>Close modal</button>
+        <button onClick={() => this.openModal()}>Open modal</button>
+        {this.state.isOpen && (
+          <div className="modal">
+            <div className="modal-body">
+              <h1>Modal title</h1>
+              <p>Modal text</p>
+              <button onClick={() => this.setState({ isOpen: false })}>
+                Close modal
+              </button>
+            </div>
           </div>
-        </div>
+        )}
       </React.Fragment>
     );
   }

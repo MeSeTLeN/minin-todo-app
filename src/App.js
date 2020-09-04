fcf6d6954b3d43aba80628dfe6f516d707f756c9
DeAdFrOnt <semenukmaks@gmail.com>
2020-09-04 21:06:47 +0300

    1-8-api fetch

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import TodoList from "./Todo/TodoList";
 import Context from "./context";
 import AddTodo from "./Todo/AddTodo";
 
 function App() {
-  const [todos, setTodos] = useState([
-    { id: 1, completed: false, title: "Shower" },
-    { id: 2, completed: true, title: "Coffee" },
-    { id: 3, completed: false, title: "Work" },
-  ]);
+  const [todos, setTodos] = useState([]);
+
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/todos?_limit=5")
+      .then((response) => response.json())
+      .then((todos) => setTodos(todos));
+  }, []);
 
   function toggleTodo(id) {
     setTodos(

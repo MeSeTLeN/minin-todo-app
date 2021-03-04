163952013ce2e2aac460e1f9df99f2b4dcf7b57f
DeAdFrOnt <semenukmaks@gmail.com>
2021-03-04 18:01:24 +0200

    feat: custom hook

--- src/Todo/AddTodo.js
+++ src/Todo/AddTodo.js
@@ -1,28 +1,40 @@
 import React, { useState } from "react";
-import PropTypes from 'prop-types'
+import PropTypes from "prop-types";
 
-function AddTodo({onCreateMethod}) {
+function useInputValue(defaultValue = "") {
   const [todoValue, setTodoValue] = useState("");
+  return {
+    bind: {
+      value: todoValue,
+      onChange: (event) => setTodoValue(event.target.value),
+    },
+    clear: () => setTodoValue(""),
+    value: () => todoValue,
+  };
+}
+
+function AddTodo({ onCreateMethod }) {
+  const input = useInputValue("");
 
   function submitMethod(event) {
-      event.preventDefault()
+    event.preventDefault();
 
-      if(todoValue.trim()){
-          onCreateMethod(todoValue)
-          setTodoValue('')
-      }
+    if (input.bind.value.trim()) {
+      onCreateMethod(input.bind.value);
+      input.clear();
+    }
   }
 
   return (
     <form style={{ marginBottom: "1rem" }} onSubmit={submitMethod}>
-      <input value={todoValue} onChange={(event)=>setTodoValue(event.target.value)} />
+      <input {...input.bind} />
       <button type="submit">Add todo</button>
     </form>
   );
 }
 
-AddTodo.propTypes={
-    onCreateMethod: PropTypes.func.isRequired
-}
+AddTodo.propTypes = {
+  onCreateMethod: PropTypes.func.isRequired,
+};
 
 export default AddTodo;

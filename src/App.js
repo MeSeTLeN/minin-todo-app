a58f1f9b3526868d14d8713db1c2e8e3c3e7096a
DeAdFrOnt <semenukmaks@gmail.com>
2021-03-06 13:48:26 +0200

    feat: loader, callback component

--- src/App.js
+++ src/App.js
@@ -1,15 +1,22 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import TodoList from "./Todo/TodoList";
 import Context from "./context";
 import AddTodo from "./Todo/AddTodo";
+import Loader from "./Loader";
 
 function App() {
-  const [todoList, setTodoList] = React.useState([]);
+  const [todoList, setTodoList] = useState([]);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     fetch("https://jsonplaceholder.typicode.com/todos?_limit=5")
       .then((response) => response.json())
-      .then((todoList) => setTodoList(todoList));
+      .then((todoList) =>
+        setTimeout(() => {
+          setTodoList(todoList);
+          setLoading(false);
+        }, 2000)
+      );
   }, []);
 
   function onChangeCompletedAppMethod(id, completed) {
@@ -45,12 +52,13 @@ function App() {
       <div className="wrapper">
         <h1>React Tutorial</h1>
         <AddTodo onCreateMethod={addTodoFunc} />
+        {loading && <Loader />}
         {todoList.length ? (
           <TodoList
             todos={todoList}
             onChangeCompletedTodoListMethod={onChangeCompletedAppMethod}
           />
-        ) : (
+        ) : loading ? null : (
           <p>No Todos</p>
         )}
       </div>

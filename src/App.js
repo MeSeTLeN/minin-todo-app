a85db86e74007885c96fd955f067ce1f74ac3414
DeAdFrOnt <semenukmaks@gmail.com>
2021-03-12 16:52:27 +0200

    feat: useEffect, fetch

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import AddTodo from "./Components/AddTodo";
 import TodoList from "./Components/TodoList";
 import Context from "./context";
 
 function App() {
-  const [todoList, setTodoList] = useState([
-    { id: 1, completed: false, title: "buy bread" },
-    { id: 2, completed: true, title: "buy milk" },
-    { id: 3, completed: false, title: "buy eggs" },
-  ]);
+  const [todoList, setTodoList] = useState([]);
+
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/todos?_limit=5")
+      .then((response) => response.json())
+      .then((todoList) => setTodoList(todoList));
+  }, []);
 
   function changeCompletedFunc(id) {
     setTodoList(

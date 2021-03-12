d503a072332fcfbf5ad36187bbabbd43f3d00ca2
DeAdFrOnt <semenukmaks@gmail.com>
2021-03-12 16:47:03 +0200

    refactor: custom hook

--- src/Components/AddTodo.js
+++ src/Components/AddTodo.js
@@ -1,23 +1,33 @@
 import React, { useState } from "react";
 
-export default function AddTodo({ onCreateMethod }) {
+function useInputValue() {
   const [todoValue, setTodoValue] = useState("");
+
+  return {
+    bind: {
+      value: todoValue,
+      onChange: (event) => setTodoValue(event.target.value),
+    },
+    value: () => todoValue,
+    clear: () => setTodoValue(""),
+  };
+}
+
+export default function AddTodo({ onCreateMethod }) {
+  const input = useInputValue();
 
   function submitMethod(event) {
     event.preventDefault();
 
-    if (todoValue.trim()) {
-      onCreateMethod(todoValue);
-      setTodoValue("");
+    if (input.value().trim()) {
+      onCreateMethod(input.value());
+      input.clear();
     }
   }
 
   return (
     <form onSubmit={submitMethod}>
-      <input
-        value={todoValue}
-        onChange={(event) => setTodoValue(event.target.value)}
-      />
+      <input {...input.bind} />
       <button type="submit">Add Todo</button>
     </form>
   );

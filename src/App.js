527e787428ae28c1b4e3b77ed665b7c9c3e009be
DeAdFrOnt <semenukmaks@gmail.com>
2021-03-15 08:20:27 +0200

    feat: Loader

--- src/App.js
+++ src/App.js
@@ -1,15 +1,20 @@
 import React, { useEffect, useState } from "react";
 import MyContext from "./context";
+import Loader from "./Loader";
 import AddTodo from "./Todo/AddTodo";
 import TodoList from "./Todo/TodoList";
 
 function App() {
   const [todos, setTodos] = useState([]);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     fetch("https://jsonplaceholder.typicode.com/todos?_limit=5")
       .then((response) => response.json())
-      .then((todos) => setTodos(todos));
+      .then((todos) => {
+        setTodos(todos);
+        setLoading(false);
+      });
   }, []);
 
   function todoCompleted(id) {
@@ -43,9 +48,10 @@ function App() {
     <MyContext.Provider value={{ removeTodo }}>
       <div className="App">
         <AddTodo onCreateTodo={addTodo} />
+        {loading && <Loader />}
         {todos.length ? (
           <TodoList todos={todos} onToggleTodo={todoCompleted} />
-        ) : (
+        ) : loading ? null : (
           <p>No todos</p>
         )}
       </div>
---
+++ src/Loader.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+export default function Loader() {
+  return (
+    <div>
+      <div className="lds-roller">
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+      </div>
+    </div>
+  );
+}

25ae05d8009d32049602e18de83b1a4c829d050e
DeAdFrOnt <semenukmaks@gmail.com>
2021-03-16 20:36:24 +0200

    feat: custom hook

--- src/Todo/AddTodo.js
+++ src/Todo/AddTodo.js
@@ -1,28 +1,40 @@
 import React, { useState } from "react";
 
-export default function AddTodo({ todos, setTodos }) {
+function useInputValue(params) {
   const [inputValue, setInputValue] = useState("");
-
-  function submitTodoHandler(e) {
-    e.preventDefault();
-
-    if (inputValue.trim()) {
-      setTodos([
-        ...todos,
-        { title: inputValue, completed: false, id: Math.random() * 1000 },
-      ]);
-    }
-    setInputValue("");
-  }
 
   function inputValueHandler(e) {
     setInputValue(e.target.value);
   }
 
+  return {
+    bind: { value: inputValue, onChange: inputValueHandler },
+    value: inputValue,
+    clear: () => setInputValue(""),
+  };
+}
+
+function AddTodo({ todos, setTodos }) {
+  const input = useInputValue();
+
+  function submitTodoHandler(e) {
+    e.preventDefault();
+
+    if (input.value.trim()) {
+      setTodos([
+        ...todos,
+        { title: input.value, completed: false, id: Math.random() * 1000 },
+      ]);
+    }
+    input.clear();
+  }
+
   return (
     <form onSubmit={submitTodoHandler}>
-      <input value={inputValue} onChange={inputValueHandler} />
+      <input {...input.bind} />
       <button type="submit">Add Todo</button>
     </form>
   );
 }
+
+export default AddTodo;
